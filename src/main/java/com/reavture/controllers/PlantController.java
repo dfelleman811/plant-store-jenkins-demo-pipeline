7d108eae1ee511afa9c0fcbc87bd97dc5d9f4454
dfelleman811 <daniel.felleman@revature.com>
2024-02-27 16:20:26 -0500

    updating requestmapping

--- src/main/java/com/reavture/controllers/PlantController.java
+++ src/main/java/com/reavture/controllers/PlantController.java
@@ -5,10 +5,12 @@ import com.reavture.repository.PlantRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RestController // @RestController -> combines @Controller along with @ResponseBody on every endpoint
+@RequestMapping("/plants")
 public class PlantController {
 
     private PlantRepository plantRepository;

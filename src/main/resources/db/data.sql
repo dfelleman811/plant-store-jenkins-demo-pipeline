17d5980e8089158840955535d53ef03e3a98143c
dfelleman811 <daniel.felleman@revature.com>
2024-02-27 15:47:19 -0500

    another push to trigger build

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,4 +4,5 @@ insert into plant (name, description, price) values
 ('my other plant','an ok plant',19.99),
 ('your other plant','the worst plant',19.99),
 ('nature plant','plant',19.99),
+('nature plant','plant',19.99),
 ('nature plant','plant',19.99);

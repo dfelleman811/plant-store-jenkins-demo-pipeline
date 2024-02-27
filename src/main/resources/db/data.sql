74477fd896ed9482c834aa5151327de667860bcb
dfelleman811 <daniel.felleman@revature.com>
2024-02-27 15:54:50 -0500

    triggering build

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -7,4 +7,5 @@ insert into plant (name, description, price) values
 ('nature plant','plant',19.99),
 ('nature plant','plant',19.99),
 ('nature plant','plant',19.99),
+('nature plant','plant',19.99),
 ('nature plant','plant',19.99);
